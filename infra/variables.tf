94e559e0bedd8b92e0b4f180d425537f564855dc
lulin_chulin <jai.rodriguezl@duocuc.cl>
2026-05-13 21:47:19 -0400

    guardar variables de region y t2

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,10 @@ variable "key_name" {
   description = "Nombre de la llave en AWS para acceder a las instancias"
   default     = "vockey"
 }
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
